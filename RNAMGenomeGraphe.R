79900c3c63f12e5358046a5afe5225a979911077
Amelia Dayton <daytonamelia@gmail.com>
2024-12-03 15:17:05 -0800

    RNAMGenomeGraphe.R working as intended

--- RNAMGenomeGraphe.R
+++ RNAMGenomeGraphe.R
@@ -17,8 +17,8 @@ plotOut <- paste(plotOut, ".jpg", sep="")
 
 # Testing variables DELETE LATER
 currentDir <- getwd()
-mutMarker_dir <- "~/bioinfo/Bi624/FW_Genetics/RNAMapout/"
-mutMarker_prefix <- "RNAmapper"
+mutMarker_dir <- "~/bioinfo/Bi624/FW_Genetics/GenomeTest/"
+mutMarker_prefix <- "Hox40_py"
 plotOut <- "Genometest"
 plotOut <- paste(plotOut, ".jpg", sep="")
 
@@ -27,29 +27,30 @@ setwd(mutMarker_dir)
 chr_list <- system(paste(
   'ls',
   'grep -v "/"',
-  'grep -E "mut.*\\.vcf"',
-  'grep -E -v "mut_.*_"',
-  'sed -E "s/.*mut_(.*)\\.vcf/\\1/"',
-  'sort -n -k1.4',
+  'grep -E "mut.*_atMarkers\\.vcf"',
+  'sed -E "s/.*mut_chr(.*)_atMarkers\\.vcf/\\1/"',
+  'sort -n -k1',
   sep = ' | '),
   intern=T)
+chr_list <- as.numeric(chr_list)
 setwd(currentDir)
 
 # Load in files
 markerFiles <- list()
 for(chr in chr_list){
   # get name of file
-  name <- paste(mutMarker_prefix,"_mut_",chr,"_atMarkers.txt",sep="")
+  name <- paste(mutMarker_prefix,"_mut_chr",chr,"_atMarkers.vcf",sep="")
   # make graph of file
-  graph <- read.delim(name, header=T)
+  graph <- read.delim(name, header=F)
   # save graph to markerFiles
-  all[[i]] <- graph[,c(2,21)]
-}
-##### put all the mutant zygosity data together into one big graph laid out by chromosome
-jpeg(paste(), width=2500, bg="white")   # setup graph environment
-par(mfrow=c(1,len(chr_list)), mar=c(5,0.5,0,0), cex.lab=4, col.axis="white")   # put all graphs into one graphspace
-# graph!
-for(chr in chr_list){
-  plot(all[[chr]][,1], all[[chr]][,2], ylim=c(0.5,1), xlim=c(0,max(all[[chr]][,1])), type="p", xlab=chr)
+  markerFiles[[chr]] <- graph[,c(2,22)]
 }
 
+# Put all the mutant zygosity data together into one big graph laid out by chromosome
+jpeg(plotOut, width=2500, bg="white")   # setup graph environment
+par(mfrow=c(1,length(chr_list)), mar=c(5,0.5,0,0), cex.lab=4, col.axis="white")   # put all graphs into one graphspace
+# graph!
+for(chr in chr_list){
+  plot(markerFiles[[chr]][,1], markerFiles[[chr]][,2], ylim=c(0.5,1), xlim=c(0,max(markerFiles[[chr]][,1])), type="p", xlab=chr)
+}
+dev.off()
