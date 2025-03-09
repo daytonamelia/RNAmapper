7e6d8a1c29b20c030c3af6b514be875993ede810
Amelia Dayton <daytonamelia@gmail.com>
2025-03-09 15:02:21 -0700

    Improved RNAmapper helper script

--- helperscripts/RNAmapper.sh
+++ helperscripts/RNAmapper.sh
@@ -2,6 +2,7 @@
 
 dir=$1
 prefix=$2
+out=${3-$dir}
 chrs=$(ls $dir | grep -v "/" | grep -E ".*mut.*\\.vcf" | sed -E "s/.*mut_chr(.*)_atMarkers\\.vcf/\\1/" | sort -n -k1)
 
 echo "RNAmapper.py running on files ${dir}${prefix}..."
@@ -11,7 +12,7 @@ do
     wt="${dir}${prefix}_wt_chr${chr}.vcf"
     mut="${dir}${prefix}_mut_chr${chr}.vcf"
     out="${dir}${prefix}"
-    ../RNAmapper.py -wt $wt -mut $mut -o $out -ch $chr &
+    ../RNAmapper.py -wt $wt -mut $mut -o $out$prefix -ch $chr &
 done
 
 wait
